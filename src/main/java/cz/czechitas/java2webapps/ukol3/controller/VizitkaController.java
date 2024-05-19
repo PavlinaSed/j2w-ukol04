52ca60a292e96962bf35977e9d1ba9c28922f561
Pavlina Sedlackova <p1sedlackova@gmail.com>
2024-05-19 23:16:08 +0200

    feat: getmapping&postmapping methods for new card added in controller feat: append & deleteById method in vizitkaService created feat: nova template created style: refactor in Vizitka.java (part 5 completed)

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,9 +1,11 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import cz.czechitas.java2webapps.ukol3.service.VizitkaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -30,4 +32,22 @@ public class VizitkaController {
         result.addObject("vizitka", service.getById(id));
         return result;
     }
+    @GetMapping("/nova")
+    public ModelAndView nova() {
+        ModelAndView result = new ModelAndView("nova");
+        return result;
+    }
+
+    @PostMapping("/nova")
+    public String append(Vizitka vizitka) {
+        service.append(vizitka);
+        return "redirect:/";
+    }
+    @PostMapping("/delete")
+    public String delete(int id) {
+        service.deleteById(id);
+        return "redirect:/";
+    }
+
+
 }
